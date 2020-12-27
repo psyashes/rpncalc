2bfc8fd7fae5ab250838f21953ee834ad3efb1cb
psyashes <43512814+psyashes@users.noreply.github.com>
2020-12-28 01:04:09 +0900

    Change cli environment to clap

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,24 @@
-use std::env;
+use clap::{Arg, App};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+    let matches = App::new("rpncalc")
+                        .version("1.0.0")
+                        .author("psyashes")
+                        .about("Calculater for Reverse Polish Notation")
+                        .arg(Arg::with_name("formula_file")
+                                .value_name("FILE")
+                                .index(1)
+                                .required(false))
+                        .arg(Arg::with_name("verbose")
+                                .short("v")
+                                .long("verbose")
+                                .required(false))
+                        .get_matches();
+
+    match matches.value_of("formula_file") {
+        Some(file) => println!("File specified : {}", file),
+        None => println!("No file specified!")
+    }
+    let verbose = matches.is_present("verbose");
+    println!("Is verbosity specified?: {}", verbose);
 }
