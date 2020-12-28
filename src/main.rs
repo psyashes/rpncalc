2ea39e7ceda7014923b82bd2966538ff654ca939
psyashes <43512814+psyashes@users.noreply.github.com>
2020-12-28 22:49:39 +0900

    Wrap clap

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,21 @@
-use clap::{Arg, App};
+use clap::Clap;
+
+#[derive(Clap, Debug)]
+#[clap(
+    name = "Reverse Polish Notation Calculater",
+    version = "1.0.0",
+    author = "psyashes",
+    about = "Reverse Polish Notation Calculater"
+)]
+
+struct Opts {
+    #[clap(short, long)]
+    verbose: bool,
+
+    #[clap(name = "FILE")]
+    formula_file: Option<String>,
+}
 
 fn main() {
-    let matches = App::new("rpncalc")
-                        .version("1.0.0")
-                        .author("psyashes")
-                        .about("Calculater for Reverse Polish Notation")
-                        .arg(Arg::with_name("formula_file")
-                                .value_name("FILE")
-                                .index(1)
-                                .required(false))
-                        .arg(Arg::with_name("verbose")
-                                .short("v")
-                                .long("verbose")
-                                .required(false))
-                        .get_matches();
-
-    match matches.value_of("formula_file") {
-        Some(file) => println!("File specified : {}", file),
-        None => println!("No file specified!")
-    }
-    let verbose = matches.is_present("verbose");
-    println!("Is verbosity specified?: {}", verbose);
+    let opts = Opts::parse();
 }
